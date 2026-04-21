b9622fa467bbe8fc483c2b6451a20002efac5a88
Elie <elie@Elie-Server.local>
2026-04-21 12:10:10 +0300

    file structure and minimal working tools

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,30 @@
-use ratatui::{
-    DefaultTerminal,
-    crossterm::event::{self, KeyCode, KeyEventKind},
-    widgets::Paragraph,
-};
+mod app;
+mod keyboard_handler;
+mod tui;
+mod ui;
 
-fn main() -> std::io::Result<()> {
-    let mut terminal = ratatui::init();
-    let result = run(&mut terminal);
-    ratatui::restore();
-    result
+use app::App;
+use ratatui::crossterm::event::{self, KeyEventKind};
+use std::io;
+
+fn main() -> io::Result<()> {
+    let mut terminal = tui::init()?;
+    let mut app = App::new();
+    let res = run(&mut terminal, &mut app);
+    tui::restore()?;
+    res
 }
 
-fn run(terminal: &mut DefaultTerminal) -> std::io::Result<()> {
-    loop {
-        terminal.draw(|frame| {
-            let text = Paragraph::new("Eli Kalter");
-            frame.render_widget(text, frame.area());
-        })?;
-
+fn run(terminal: &mut tui::Tui, app: &mut App) -> io::Result<()> {
+    while !app.should_quit {
+        terminal.draw(|f| ui::render(f, app))?;
         if event::poll(std::time::Duration::from_millis(16))? {
             if let event::Event::Key(key) = event::read()? {
-                if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
-                    return Ok(());
+                if key.kind == KeyEventKind::Press {
+                    keyboard_handler::handle_key_events(key, app);
                 }
             }
         }
     }
+    Ok(())
 }
