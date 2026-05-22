90655d8a97cb694a9229e43401b39341df0f8c10
Elie <elie@Elie-Server.tailae0d7b.ts.net>
2026-05-23 01:35:49 +0300

    Bach to basics

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,21 @@
-mod app;
-mod keyboard_handler;
-mod tui;
-mod ui;
+fn main() {
+    let mut stone_count = 1;
 
-use app::App;
-use ratatui::crossterm::event::{self, KeyEventKind};
-use std::io;
-
-fn main() -> io::Result<()> {
-    let mut terminal = tui::init()?;
-    let mut app = App::new();
-    let res = run(&mut terminal, &mut app);
-    tui::restore()?;
-    res
-}
-
-fn run(terminal: &mut tui::Tui, app: &mut App) -> io::Result<()> {
-    while !app.should_quit {
-        terminal.draw(|f| ui::render(f, app))?;
-        if event::poll(std::time::Duration::from_millis(16))? {
-            if let event::Event::Key(key) = event::read()? {
-                if key.kind == KeyEventKind::Press {
-                    keyboard_handler::handle_key_events(key, app);
-                }
-            }
+    loop {
+        let oddity = stone_count % 2;
+        if oddity == 0 {
+            println!("The sun is setting");
+        } else {
+            println!("The sun is rising");
+        }
+        roll_stone(stone_count);
+        stone_count += 1;
+        if stone_count == 10 {
+            break;
         }
     }
-    Ok(())
+}
+
+fn roll_stone(stone_number: i32) {
+    println!("The stone count is {}", stone_number);
 }
